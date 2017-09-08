8f290e6da0b30aff7daf21a3c7c130f64e45e8d2
ahmd1 albasrowe <ahmdalbsrawe@gmail.com>
2017-09-09 01:56:23 +0300

    ferst sisharb app
    this is my frest c sharb prougram

---
+++ ferst app - Copy/ferst app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ferst_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//this is my ferst me si sharb
+            Console.WriteLine("hilo wourd");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ferst vareabl/ferst vareabl/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ferst_vareabl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 25;
+            Console.WriteLine ("My Variable Value ={0}",X);
+            Console.ReadKey();
+        }
+    }
+}
